1d12188049d5eb2143e7900cd8fe2dc4897c92f9
klemavilanova <klemavilanova@atanasoff17.danielcastelao.org>
2016-10-27 08:56:08 +0200

    commit2

--- Documentos/practicacontornos/src/practicacontornos/Practicacontornos.java
+++ Documentos/practicacontornos/src/practicacontornos/Practicacontornos.java
@@ -17,6 +17,7 @@ public class Practicacontornos {
     public static void main(String[] args) {
         // TODO code application logic here
         int commit1;
+        int commit2;
         
     }
     
